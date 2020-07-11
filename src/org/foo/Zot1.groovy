4fd031896a5f7c1d53c830eb5e9131d53a70e193
mahesh288646 <mahesh288646@gmail.com>
2020-07-11 13:36:58 -0400

    sample

--- src/org/foo/Zot1.groovy
+++ src/org/foo/Zot1.groovy
@@ -1,7 +1,7 @@
 package org.foo
 
 def checkOutFrom(repo) {
-  git branch: "master", credentialsId: 'Git-ID', url: "git@github.com:mahesh288646/${repo}"
+  git branch: "master", credentialsId: 'Git-ID', url: "https://github.com/mahesh288646/${repo}.git"
 }
 
 return this
