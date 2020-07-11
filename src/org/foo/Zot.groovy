cf496314ff84a8834eeb5b2fff48f3ff3474fca3
mahesh288646 <mahesh288646@gmail.com>
2020-07-11 13:27:21 -0400

    sample

--- src/org/foo/Zot.groovy
+++ src/org/foo/Zot.groovy
@@ -1,12 +1,12 @@
 package org.foo
 
 def checkOutFrom(repo) {
-//  git credentialsId: 'Git-ID' url: "git@github.com:mahesh288646/${repo}"
-        github {
-            id('123') // IMPORTANT: use a constant and unique identifier
-            scanCredentialsId('Git-ID')
-            repoOwner('mahesh288646')
-            repository(${repo})
+  git branch: master, credentialsId: 'Git-ID', url: "git@github.com:mahesh288646/${repo}"
+//        github {
+//            id('123') // IMPORTANT: use a constant and unique identifier
+//            scanCredentialsId('Git-ID')
+//            repoOwner('mahesh288646')
+//            repository(${repo})
         }
 }
 
