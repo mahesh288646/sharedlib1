5640cd4e9bef163ca09808afac324cba5bd10431
mahesh288646 <mahesh288646@gmail.com>
2020-07-11 23:32:16 -0400

    sample

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,11 +1,12 @@
 def call(def server, def port) {
 //    httpRequest httpMode: 'POST', url: "http://${server}:${port}/shutdown", validResponseCodes: '200,408'
-    sshagent(['Jenkins-RemoteCredentials']) {
-        sh "scp -vvv target/*.jar wasadmin@${server}:/opt/"
+//    sshagent(['Jenkins-RemoteCredentials']) {
+//        sh "scp -vvv target/*.jar wasadmin@${server}:/opt/"
+	sh "cp target/*.jar wasadmin@${server}:/opt/"
         //sh "ssh root@${server} nohup java -Dserver.port=${port} -jar /opt/jenkins-demo.jar &"
     }
     retry (3) {
         sleep 5
-        httpRequest url:"http://${server}:${port}/health", validResponseCodes: '200', validResponseContent: '"status":"UP"'
+        //httpRequest url:"http://${server}:${port}/health", validResponseCodes: '200', validResponseContent: '"status":"UP"'
     }
 }
